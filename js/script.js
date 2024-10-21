84f939304512959dfa47193ce835acc71c5c8693
provash10 <provash20cb@gmail.com>
2024-10-21 21:51:12 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Class 29 Practice @JS Array []
+// ============================
+
+// 1. 1D Array (Index Array)
+// Array =>>> indexArray[];
+
+let indexArray = [43, 98, 34, 56, 33, 65];
+// Array =>>> indexArray[4];
+console.log(indexArray[4]);
+console.log(indexArray[1]);
+console.log(indexArray[3]);
+// =============================
+
+// 3. Associative Array (Object) {}
+// student.age;
+
+let name = "Rahim";
+let age  = 12;
+let blood= "O+";
+
+let student = {name: "Rahim", age: 12, blood: "O+"};
+
+// student.age;
+console.log(student.age);
+console.log(student.name);
+
+// ======================================
+
+// 2. 2D Array ()
+
+// 39= 1 no row, 2 no colmn
+
+let twoDimensionalArray = [
+    [43, 98, 32, 2],
+    [11, 81, 39, 47],
+    [12, 5, 100, 77],
+    [91, 1, 2, 0]
+];
+
+// 39 where?
+// twoDimensionalArray[1][2]; //39
+
+console.log(twoDimensionalArray[1][2]);
+
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
+
